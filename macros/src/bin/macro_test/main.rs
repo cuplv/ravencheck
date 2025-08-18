e49a686ff835b04f1daeded876725f0185b1b9c5
octalsrc <contact@octalsrc.org>
2025-08-18 16:03:40 -0600

    Add Cargo.toml fields for packaging, clean up

--- macros/src/bin/macro_test/main.rs
+++
@@ -1,9 +0,0 @@
-use ravencheck_macros::verify;
-use ravenlang::CheckedSig;
-
-fn main() {
-    let mut sig = CheckedSig::empty();
-    sig.add_sort("Nat");
-
-    verify!(&sig, forall(|a:Nat| a == a));
-}
