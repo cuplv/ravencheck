3bbd28e1f5f9fbbbc617c9450a49be6a8701de9d
octalsrc <nicklewchenko92@gmail.com>
2025-08-20 16:16:22 -0600

    Update example

--- examples/sets.rs
+++ examples/sets.rs
@@ -9,93 +9,81 @@
 // the verification-specific code is erased when compiling for
 // release.
 #[ravencheck::check_module]
+// This command declares the uninterpreted type 'u32' to the solver.
+#[declare_types(u32)]
 pub mod my_mod {
     use std::collections::HashSet;
 
     // The 'declare' attribute tells the solver that we have an
-    // uninterpreted sort called 'Set'.
+    // uninterpreted type called 'MySet'.
     #[declare]
-    #[derive(PartialEq, Eq, Clone)]
-    pub struct Set(pub HashSet<u32>);
+    pub type MySet = HashSet<u32>;
 
     // Here, 'declare' tells the solver that we have an uninterpreted
-    // constant called 'empty_set' with sort 'Set'.
+    // constant called 'empty_set' with type 'MySet'.
     #[declare]
-    pub fn empty_set() -> Set {
-        Set(HashSet::new())
+    pub fn empty_set() -> MySet {
+        HashSet::new()
     }
 
-    // Declare an uninterpreted 'Elem' sort.
-    #[declare]
-    #[derive(PartialEq, Eq, Clone, Copy)]
-    pub struct Elem(u32);
-
     // Declare an uninterpreted relation (boolean-output function) on
-    // 'Elem' and 'Set'.
+    // 'u32' and 'MySet'.
     #[declare]
-    pub fn member(e: Elem, s: Set) -> bool {
-        s.0.contains(&e.0)
+    pub fn member(e: u32, s: MySet) -> bool {
+        s.contains(&e)
     }
 
     // The 'assume' attribute gives the body of the following function
     // to the solver as an axiom.
     //
     // This function body is erased before the Rust compiler is
-    // called, and so we can use special logical syntax like
-    // forall(..) and exists(..).
+    // called, so we can use special logical syntax like forall(..)
+    // and exists(..).
     #[assume]
     fn equal_or_distinguisher() -> bool {
-        forall(|a:Set,b:Set| {
-            a == b || exists(|e:Elem| member(e,a) !=  member(e,b))
+        forall(|a:MySet,b:MySet| {
+            a == b || exists(|e:u32| member(e,a) !=  member(e,b))
         })
     }
 
     #[assume]
     fn empty_set_no_member() -> bool {
-        forall(|e: Elem| {
+        forall(|e: u32| {
             !member(e, empty_set())
         })
     }
 
-    // Declare an uninterpreted function on 'Set'.
+    // Declare an uninterpreted function on 'MySet'.
     #[declare]
-    pub fn union(a: Set, b: Set) -> Set {
-        let Set(s1) = a;
-        let Set(s2) = b;
-        let s3 = s1.union(&s2).cloned().collect();
-        Set(s3)
+    pub fn union(a: MySet, b: MySet) -> MySet {
+        a.union(&b).cloned().collect()
     }
 
     // This is a special form of 'assume' that uses the function body
     // as an axiom on (a,b,c) that must be true when union(a,b) = c.
     #[annotate(union)]
-    fn union_def(a: Set, b: Set, c: Set) -> bool {
-        forall(|e: Elem| {
+    fn union_def() -> bool {
+        // Function input arguments
+        |a: MySet, b: MySet|
+        // Function output
+        |c: MySet|
+        // Condition that relates the inputs and output
+        forall(|e: u32| {
             member(e,c) == (member(e,a) || member(e,b))
         })
     }
 
     // The 'verify' attribute gives the following function body to the
     // solver as a verification goal, which it checks based on the
-    // axioms it has received so-far.
+    // axioms it has received so far.
     //
     // The #[verify] attribute is analogous to the #[test] attribute
     // in an ordinary Rust testing module.
     #[verify]
-    fn union_preserves_member() -> bool {
-        forall(|e:Elem,s:Set| {
-            !member(e,s) || member(e, union(s,s))
+    fn union_idempotent() -> bool {
+        forall(|a: MySet, b: MySet| {
+            union(union(a,b), b) != union(a,b)
         })
-    }
-
-    #[verify]
-    fn union_self_is_self() -> bool {
-        forall(|s:Set| union(s,s) == s)
-    }
-
-    #[verify]
-    fn union_empty_set() -> bool {
-        union(empty_set(), empty_set()) == empty_set()
     }
 
     // Here are ordinary Rust tests, which are checked by compiling
@@ -104,7 +92,7 @@ pub mod my_mod {
     // Calling 'cargo test' will run these tests alongside the
     // verification process for the #[verify] properties above.
     #[cfg(test)]
-    mod runtime_properties {
+    mod normal_tests {
         use super::*;
 
         // Since this is real Rust code that will be executed, we
@@ -123,20 +111,16 @@ pub mod my_mod {
         #[test]
         fn empty_is_empty() {
             assert!(
-                !member(Elem(1), empty_set())
+                !member(1, empty_set())
             )
         }
     }
 }
 
+// Now we can use the definitions from above the module in normal Rust
+// code.
 fn main() {
     let s = my_mod::empty_set();
     let result = my_mod::union(s.clone(), s.clone()) == s;
     println!("Result is {}", result);
 }
-
-// Note that, since this file is built as an "example" by Cargo, the
-// tests and verification code are not actually executed by 'cargo
-// test'. If you want to play with and modify some examples, use the
-// ones in src/macro_examples/*, which are actually tested when you
-// run 'cargo test'.
--- src/macro_examples/sets_using_alias.rs
+++ src/macro_examples/sets_using_alias.rs
@@ -1,21 +1,45 @@
+// This is the main macro call that tells ravencheck to use
+// verification in this module.
+//
+// Because this example file is inside the ravencheck library, we call
+// it using 'crate'.
+//
+// When using it in your own library, you would call it as:
+//
+//     #[ravencheck::check_module]
+//     mod my_mod { .. }
+//
 #[crate::check_module(crate)]
+// This command declares the uninterpreted type 'u32' to the solver.
 #[declare_types(u32)]
 pub mod my_mod {
     use std::collections::HashSet;
 
+    // The 'declare' attribute tells the solver that we have an
+    // uninterpreted type called 'MySet'.
     #[declare]
     type MySet = HashSet<u32>;
 
+    // Here, 'declare' tells the solver that we have an uninterpreted
+    // constant called 'empty_set' with type 'MySet'.
     #[declare]
     fn empty_set() -> MySet {
         HashSet::new()
     }
 
+    // Declare an uninterpreted relation (boolean-output function) on
+    // 'u32' and 'MySet'.
     #[declare]
     fn member(e: u32, s: MySet) -> bool {
         s.contains(&e)
     }
 
+    // The 'assume' attribute gives the body of the following function
+    // to the solver as an axiom.
+    //
+    // This function body is erased before the Rust compiler is
+    // called, so we can use special logical syntax like forall(..)
+    // and exists(..).
     #[assume]
     fn equal_or_distinguisher() -> bool {
         forall(|a:MySet,b:MySet| {
@@ -30,31 +54,54 @@ pub mod my_mod {
         })
     }
 
+    // Declare an uninterpreted function on 'MySet'.
     #[declare]
     fn union(a: MySet, b: MySet) -> MySet {
         a.union(&b).cloned().collect()
     }
 
+    // This is a special form of 'assume' that uses the function body
+    // as an axiom on (a,b,c) that must be true when union(a,b) = c.
     #[annotate(union)]
     fn union_def() -> bool {
+        // Function input arguments
         |a: MySet, b: MySet|
+        // Function output
         |c: MySet|
+        // Condition that relates the inputs and output
         forall(|e: u32| {
             member(e,c) == (member(e,a) || member(e,b))
         })
     }
 
+    // The 'verify' attribute gives the following function body to the
+    // solver as a verification goal, which it checks based on the
+    // axioms it has received so far.
+    //
+    // The #[verify] attribute is analogous to the #[test] attribute
+    // in an ordinary Rust testing module.
     #[verify]
     fn union_idempotent() -> bool {
         forall(|a: MySet, b: MySet| {
-            union(union(a,b), b) != union(a,b)
+            union(union(a,b), b) == union(a,b)
         })
     }
 
+    // Here are ordinary Rust tests, which are checked by compiling
+    // them using the Rust compiler and then executing the results.
+    //
+    // Calling 'cargo test' will run these tests alongside the
+    // verification process for the #[verify] properties above.
     #[cfg(test)]
     mod normal_tests {
         use super::*;
 
+        // Since this is real Rust code that will be executed, we
+        // can't use logical quantifiers like forall(..) and
+        // exists(..).
+        //
+        // Compare this test to the 'union_self_is_self' verification
+        // property above.
         #[test]
         fn union_empty_set() {
             assert!(
