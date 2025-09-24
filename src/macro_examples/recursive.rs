86c3742754a0b15dfd7a21e1731381437912cc27
octalsrc <contact@octalsrc.org>
2025-09-23 20:05:27 -0600

    Add recursive example case

--- src/macro_examples/recursive.rs
+++ src/macro_examples/recursive.rs
@@ -22,13 +22,32 @@ mod rvn {
         }
     }
 
-    // #[define_rec]
-    // fn concat<E>(e_def: E, l1: List<E>, l2: List<E>) -> List<E> {
-    //     if l1 == empty_list::<E>() {
-    //         l2
-    //     } else {
-    //         let (e, l1_tail) = uncons::<E>(e_def, l);
-    //         cons(e, concat(l1_tail, l2))
-    //     }
-    // }
+    #[define]
+    #[recursive]
+    fn concat<E>(e_def: E, l1: List<E>, l2: List<E>) -> List<E>
+    where E: Clone + PartialEq
+    {
+        if l1 == empty_list::<E>() {
+            l2
+        } else {
+            let (e, l1_tail) = uncons::<E>(e_def.clone(), l1);
+            cons(e, concat::<E>(e_def, l1_tail, l2))
+        }
+    }
+
+    #[annotate(concat::<T>(e, l1, l2) => l3)]
+    fn left_empty() -> bool {
+        implies(
+            l1 == empty_list::<E>(),
+            l3 == l2,
+        )
+    }
+
+    #[annotate(concat::<T>(e, l1, l2) => l3)]
+    fn right_empty() -> bool {
+        implies(
+            l2 == empty_list::<E>(),
+            l3 == l1,
+        )
+    }
 }
