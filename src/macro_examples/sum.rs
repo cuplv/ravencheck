223ac1cbb158d3f91075095815e9134857b7ecdd
octalsrc <contact@octalsrc.org>
2025-09-26 10:59:52 -0600

    Check additional enum examples

--- src/macro_examples/sum.rs
+++ src/macro_examples/sum.rs
@@ -113,23 +113,28 @@ mod rvn {
         })
     }
 
-    // #[falsify]
-    // fn symbolic2() -> bool {
-    //     forall(|l1: List<usize>, l2: List<usize>| {
-    //         match l1 {
-    //             List::<usize>::Cons(n, tail) => n == n && tail == l2,
-    //             List::<usize>::Nil => true,
-    //         }
-    //     })
-    // }
+    #[falsify]
+    fn symbolic2() -> bool {
+        forall(|l1: List<usize>, l2: List<usize>| {
+            match l1 {
+                List::<usize>::Cons(n, tail) => n == n && tail == l2,
+                List::<usize>::Nil => true,
+            }
+        })
+    }
 
-    // #[falsify]
-    // fn symbolic3() -> bool {
-    //     forall(|l1: List<usize>| {
-    //         match l1 {
-    //             List::<usize>::Cons(n, tail) => n == n,
-    //             List::<usize>::Nil => false,
-    //         }
-    //     })
-    // }
+    #[falsify]
+    fn symbolic3() -> bool {
+        forall(|l1: List<usize>| {
+            match l1 {
+                List::<usize>::Cons(n, tail) => {
+                    match l1 {
+                        List::<usize>::Cons(n, tail) => true,
+                        List::<usize>::Nil => true,
+                    }
+                }
+                List::<usize>::Nil => false,
+            }
+        })
+    }
 }
