d49b61e43d424615dcd7d3a3c2d82d0835003c4f
octalsrc <contact@octalsrc.org>
2025-11-03 11:37:46 -0700

    Work on documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,46 @@
+/*!
+# Example
+
+```
+#[ravencheck::check_module]
+#[declare_types(u32)]
+mod rvn {
+    #[declare]
+    const ZERO: u32 = 0;
+
+    #[declare]
+    fn le(a: u32, b: u32) -> bool {
+        a <= b
+    }
+
+    #[assume]
+    fn le_anti_symmetric() -> bool {
+        forall(|x: u32, y: u32| {
+            implies(
+                le(x,y) && le(y,x),
+                x == y
+            )
+        })
+    }
+
+    #[assume]
+    fn zero_is_least() -> bool {
+        forall(|x: u32| le(ZERO, x))
+    }
+
+    #[verify]
+    fn prop1() -> bool {
+        forall(|x: u32| {
+            implies(
+                le(x, ZERO),
+                x == ZERO
+            )
+        })
+    }
+}
+```
+*/
+
 #[cfg(test)]
 mod macro_examples;
 
