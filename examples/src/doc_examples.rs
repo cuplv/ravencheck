a6d3b92fe6a05b1b7e9d6960b5d0961a8e4be006
octalsrc <contact@octalsrc.org>
2025-11-10 11:26:00 -0700

    Doc examples

--- examples/src/doc_examples.rs
+++ examples/src/doc_examples.rs
@@ -39,3 +39,43 @@ pub mod rvn_u32 {
         le(x, ZERO)
     }
 }
+
+#[ravencheck::export_module]
+#[declare_types(HashSet<_>)]
+pub mod rvn_hashset {
+    pub use std::collections::HashSet;
+    use std::hash::Hash;
+
+    #[declare]
+    pub fn member<T>(elem: &T, set: &HashSet<T>) -> bool
+    where T: Eq + Hash
+    {
+        set.contains(elem)
+    }
+
+    #[assume]
+    #[inst_for(HashSet<T> => <T>)]
+    fn member_defines_eq<T>(s1: HashSet<T>, s2: HashSet<T>) -> bool {
+        let some_difference = exists(|e: T| {
+            member::<T>(e,s1) != member::<T>(e,s2)
+        });
+
+        s1 == s2 || some_difference
+    }
+
+    #[declare]
+    pub fn insert<T>(elem: T, mut set: HashSet<T>) -> HashSet<T>
+    where T: Eq + Hash
+    {
+        set.insert(elem);
+        set
+    }
+
+    #[assume(insert::<T>(elem1, set1) => set2)]
+    fn insert_def() -> bool {
+        forall(|elem2: T| {
+            member::<T>(elem2, set2) ==
+                (member::<T>(elem2, set1) || elem1 == elem2)
+        })
+    }
+}
