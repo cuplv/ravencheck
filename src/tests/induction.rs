358109578ef587d717d0cb27a8faccb1550bd699
octalsrc <contact@octalsrc.org>
2026-01-27 11:05:08 -0700

    Add tests for annotate_multi

---
+++ src/tests/induction.rs
@@ -0,0 +1,59 @@
+#[crate::check_module(crate)]
+mod induction {
+    #[define]
+    enum Nat { Z, S(Box<Nat>) }
+
+    #[define]
+    #[recursive]
+    fn add(x: Nat, y: Nat) -> Nat {
+        match x {
+            Nat::Z => y,
+            Nat::S(x_sub) => Nat::S(Box::new(add(*x_sub, y))),
+        }
+    }
+
+    #[annotate_multi]
+    #[for_values(x: Nat, y: Nat, sy: Nat)]
+    #[for_call(add(x,y) => xy)]
+    #[for_call(add(x,sy) => xsy)]
+    fn factor_s_right_good() -> Bool {
+        implies(
+            sy == Nat::S(y),
+            Nat::S(xy) == xsy
+        )
+    }
+
+    #[annotate_multi]
+    #[should_fail]
+    #[for_values(x: Nat, y: Nat, sy: Nat)]
+    #[for_call(add(x,y) => xy)]
+    #[for_call(add(x,sy) => xsy)]
+    fn factor_s_right_oops() -> Bool {
+        implies(
+            sy == Nat::S(y),
+            // This line uses != in place of ==
+            Nat::S(xy) != xsy
+        )
+    }
+
+    #[annotate_multi]
+    #[should_fail]
+    #[for_values(x: Nat, y: Nat, z: Nat)]
+    #[for_call(add(x,x) => xx)]
+    fn prop_0() -> bool {
+        // Should obviously fail.
+        add(y,y) == add(z,z)
+    }
+
+    #[annotate_multi]
+    #[should_fail]
+    #[for_values(x: Nat)]
+    #[for_call(add(x,x) => xx)]
+    fn prop_1() -> bool {
+        // Here is a second version that should fail, which quantifies
+        // y and z in a different way.
+        forall(|y: Nat, z: Nat| {
+            add(y,y) == add(z,z)
+        })
+    }
+}
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,4 +1,5 @@
 mod bool_fun;
 mod cycle_checking;
+mod induction;
 mod matching;
 mod misc;
